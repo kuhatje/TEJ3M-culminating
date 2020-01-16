e3a217136b07ed7f4a089ea24936ca307a74a320
Eric Pei <crackersamdjam@gmail.com>
2020-01-16 14:54:44 -0500

    upload

--- test.py
+++ test.py
@@ -15,56 +15,56 @@ GPIO.setup(right_high, GPIO.OUT)
 GPIO.setup(right_low, GPIO.OUT)
 
 def run():
-    GPIO.output(left_high, l1)
-    GPIO.output(left_low, l2)
-    GPIO.output(right_high, r1)
-    GPIO.output(right_low, r2)
+	print('set')
+	GPIO.output(left_high, l1)
+	GPIO.output(left_low, l2)
+	GPIO.output(right_high, r1)
+	GPIO.output(right_low, r2)
 
 def forward():
-    l1 = r2 = False
-    l2 = r1 = True
-    run()
-    #GPIO.output(left_high, False)
-    #GPIO.output(left_low, True)
-    #GPIO.output(right_high, True)
-    #GPIO.output(right_low, False)
+	l1 = r2 = False
+	l2 = r1 = True
+	run()
+	#GPIO.output(left_high, False)
+	#GPIO.output(left_low, True)
+	#GPIO.output(right_high, True)
+	#GPIO.output(right_low, False)
 def backward():
-    l1 = r2 = True
-    l2 = r1 = False
-    run()
+	l1 = r2 = True
+	l2 = r1 = False
+	run()
 def turnleft():
-    l2 = r2 = False
-    l1 = r1 = True
-    run()
+	l2 = r2 = False
+	l1 = r1 = True
+	run()
 def turnright():
-    l2 = r2 = True
-    l1 = r1 = False
-    run()
+	l2 = r2 = True
+	l1 = r1 = False
+	run()
 def halt():
-    l1 = l2 = r1 = r2 = False
-    run()
+	l1 = l2 = r1 = r2 = False
+	run()
 
 pygame.init()
 display = pygame.display.set_mode((200, 200))
 display.fill((0, 0, 0))
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                running = False
-                break
-            if event.key == pygame.K_w:
-                forward()
-            elif event.key == pygame.K_s:
-                backward()
-            elif event.key == pygame.K_a:
-                turnleft()
-            elif event.key == pygame.K_d:
-                turnright()
-            else
-                halt()
-    #add some sort of delay to slow down
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+				running = False
+			if event.key == pygame.K_w:
+				forward()
+			elif event.key == pygame.K_s:
+				backward()
+			elif event.key == pygame.K_a:
+				turnleft()
+			elif event.key == pygame.K_d:
+				turnright()
+			else:
+				halt()
+	#add some sort of delay to slow down
 
 print('start')
 print('end')
